60d3602180b311f5ab5c4cc24609e3b4d55cd038
Webregrets <mikael.westerberg91@gmail.com>
2022-01-14 06:47:22 +0100

    Added function to get the new win11 context menu back if program is ran and the registry key exists.
    Added auto exit after five secs
    
    Did som minor cleanup

--- OldContextMenu/Program.cs
+++ OldContextMenu/Program.cs
@@ -3,32 +3,35 @@ using System.Diagnostics;
 
 Console.WriteLine("Let's change the context menu!");
 
-
-
-//RegistryKey exist = Registry.CurrentUser.Name;
-
 RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32");
 
-
-if (key != null)
+if (key == null)
 {
     OldContext();
-    
-    Console.WriteLine("");
 }
 else
 {
     NewContext();
-    Console.WriteLine("");
 }
 
-Console.ReadKey();
+//Gets all processes with "explorer" as name. Since there probably is only one. Is there a GetProcessByName? probably
+Process[] explorer = Process.GetProcessesByName("explorer");
 
+//kills Explorer.exe since needed for the context menu to update
+explorer[0].Kill();
 
+Process process = new Process();
+process.StartInfo.FileName = "explorer";
+process.StartInfo.UseShellExecute = true;
+process.Start();
+
+Console.WriteLine("Did some registry changes. Try right-clicking a file!");
+
+Console.WriteLine("This window closes in five seconds");
+
+System.Threading.Thread.Sleep(5000);    
 
 // HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32
-
-
 
 static void OldContext()
 {
@@ -41,27 +44,27 @@ static void OldContext()
         //Adds the "(Default)" value and sets data to "" instead of "not defined" 
         keyIn.SetValue("", "");
         keyIn.Close();
+        Console.WriteLine("Created regkey");
+
     }
     catch (Exception ex)
     {
         Console.WriteLine(ex);
     }
-
-    //Gets all processes with "explorer" as name. Since there probably is only one. Is there a GetProcessByName? probably
-    Process[] explorer = Process.GetProcessesByName("explorer");
-
-    //kills Explorer.exe since needed for the context menu to update
-    explorer[0].Kill();
-
-    Process process = new Process();
-    process.StartInfo.FileName = "explorer";
-    process.StartInfo.UseShellExecute = true;
-    process.Start();
-
-    Console.WriteLine("Added registry key to get the old context menu back. Try right-clicking a file!");  
+    
 }
 
 static void NewContext()
 {
+    try
+    {
+        Console.WriteLine(@"Deleting HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2} and subkeys");
 
+        Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}");
+        Console.WriteLine("Deleted regkey");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
 }
