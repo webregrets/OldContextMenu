cbf66a75c613fc4498b8ffb03bc01c4bbcfb7e72
Webregrets <mikael.westerberg91@gmail.com>
2022-01-14 23:16:42 +0100

    Separated functions to new file
    Added OS-detection

---
+++ OldContextMenu/Functions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+
+namespace OldContextMenu
+{
+    public class Functions
+    {
+
+        public static void OldContext()
+        {
+
+            try
+            {
+                Console.WriteLine(@"Creating subkey HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32");
+                RegistryKey keyIn = Registry.CurrentUser.CreateSubKey(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32");
+
+                keyIn.SetValue("", "");
+                keyIn.Close();
+                Console.WriteLine("Created regkey");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        public static void NewContext()
+        {
+            try
+            {
+                Console.WriteLine(@"Deleting HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2} and subkeys");
+
+                Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}");
+                Console.WriteLine("Deleted regkey");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static bool PreCheck()
+        {
+            string osVer = Environment.OSVersion.ToString();
+
+            string osString = osVer.Substring(0, 28);
+
+            if (osString == "Microsoft Windows NT 10.0.22") 
+            {
+                Console.WriteLine("OS detected as: " + osVer);
+                return true; 
+            }
+            else 
+            {
+                Console.WriteLine("OS detected as: " + osVer);
+                return false; 
+            }
+
+        }
+
+
+
+    }
+}
--- OldContextMenu/Program.cs
+++ OldContextMenu/Program.cs
@@ -1,70 +1,49 @@
 ﻿using Microsoft.Win32; 
 using System.Diagnostics;
 
-Console.WriteLine("Let's change the context menu!");
+using OldContextMenu;
 
-RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32");
 
-if (key == null)
+bool ok = Functions.PreCheck();
+
+if (ok)
 {
-    OldContext();
+
+    Console.WriteLine("Let's change the context menu!");
+
+    RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32");
+
+
+    if (key == null)
+    {
+        Functions.OldContext();
+    }
+    else
+    {
+        Functions.NewContext();
+    }
+
+    //Gets all processes with "explorer" as name. Since there probably is only one. Is there a GetProcessByName? probably
+    Process[] explorer = Process.GetProcessesByName("explorer");
+
+    //kills Explorer.exe since needed for the context menu to update
+    explorer[0].Kill();
+
+    Process process = new Process();
+    process.StartInfo.FileName = "explorer";
+    process.StartInfo.UseShellExecute = true;
+    process.Start();
+
+    Console.WriteLine("Did some registry changes. Try right-clicking a file!");
+    Console.WriteLine("This window closes in five seconds");
+
+    System.Threading.Thread.Sleep(5000);
+
 }
 else
 {
-    NewContext();
-}
+    Console.WriteLine("You are not running Windows 11. No menu-switching for you my friend!");
+    Console.WriteLine("This window closes in five seconds");
 
-//Gets all processes with "explorer" as name. Since there probably is only one. Is there a GetProcessByName? probably
-Process[] explorer = Process.GetProcessesByName("explorer");
-
-//kills Explorer.exe since needed for the context menu to update
-explorer[0].Kill();
-
-Process process = new Process();
-process.StartInfo.FileName = "explorer";
-process.StartInfo.UseShellExecute = true;
-process.Start();
-
-Console.WriteLine("Did some registry changes. Try right-clicking a file!");
-
-Console.WriteLine("This window closes in five seconds");
-
-System.Threading.Thread.Sleep(5000);    
-
-// HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32
-
-static void OldContext()
-{
-
-    try
-    {
-        Console.WriteLine(@"Creating subkey HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32");
-        RegistryKey keyIn = Registry.CurrentUser.CreateSubKey(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32");
-
-        //Adds the "(Default)" value and sets data to "" instead of "not defined" 
-        keyIn.SetValue("", "");
-        keyIn.Close();
-        Console.WriteLine("Created regkey");
-
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex);
-    }
-    
-}
-
-static void NewContext()
-{
-    try
-    {
-        Console.WriteLine(@"Deleting HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2} and subkeys");
-
-        Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}");
-        Console.WriteLine("Deleted regkey");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex);
-    }
+    System.Threading.Thread.Sleep(5000);
 }
